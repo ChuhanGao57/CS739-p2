60fb874a6c27778ab9d74f1d775344979556f397
ChuhanGao57 <gaochthu@gmail.com>
2019-11-20 20:02:33 -0600

    created rpctest branch

--- src/main/java/DHT/DHTMain.java
+++ src/main/java/DHT/DHTMain.java
@@ -8,12 +8,6 @@ public class DHTMain {
 
     public static void main(String[] args) {
         // TODO: implement
-        server = new DHTServer("localhost", 8888);
-        try {
-            server.start();
-            System.out.println(server.findSuccessor(123456).toString());
-        } catch (IOException e) {
-            //TODO: handle exception
-        }
+        System.out.println("Main");
     } 
 }
