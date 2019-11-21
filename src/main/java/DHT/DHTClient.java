a49e4cba227e5ffcb8e1f5ac942bd2d9d7ed91ed
ChuhanGao57 <gaochthu@gmail.com>
2019-11-21 10:34:29 -0600

    played around with rpc

--- src/main/java/DHT/DHTClient.java
+++ src/main/java/DHT/DHTClient.java
@@ -13,8 +13,51 @@ import java.net.InetSocketAddress;
  */
 public class DHTClient {
     private static final Logger logger = Logger.getLogger(DHTClient.class.getName());
+    private final ManagedChannel channel;
+    private final DHTRpcGrpc.DHTRpcBlockingStub blockingStub;
 
-    public static void main(String[] args) {
-        System.out.println("fuck you!");
+    public DHTClient(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port)
+        // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+        // needing certificates.
+        .usePlaintext()
+        .build());
+    }
+
+    /** Construct client for accessing HelloWorld server using the existing channel. */
+    DHTClient(ManagedChannel channel) {
+        this.channel = channel;
+        blockingStub = DHTRpcGrpc.newBlockingStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    public void findSuccessor(int id) {
+        logger.info("Will try to contact " + id + " ...");
+        FindSuccessorRequest request = FindSuccessorRequest.newBuilder().setId(id).build();
+        FindSuccessorReply reply;
+        try {
+            reply = blockingStub.findSuccessor(request);
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return;
+        }
+        System.out.println("Received " + reply.getAddress() + ":" +reply.getPort());
+    }
+
+    public static void main(String[] args) throws Exception {
+        DHTClient client = new DHTClient("localhost", 9001);
+        try {
+            String user = "world";
+            // Use the arg as the name to greet if provided
+            if (args.length > 0) {
+                user = args[0];
+            }
+            client.findSuccessor(123);
+            } finally {
+                client.shutdown();
+        }
     }
 }
--- src/main/java/DHT/DHTServer.java
+++ src/main/java/DHT/DHTServer.java
@@ -12,7 +12,9 @@ import java.util.HashMap;
  * Server that manages startup/shutdown of a {@code Greeter} server.
  */
 public class DHTServer {
-    
+    private static final Logger logger = Logger.getLogger(DHTServer.class.getName());
+
+    private Server server;
 
     public static final int M = 32;
 
@@ -22,8 +24,54 @@ public class DHTServer {
     private InetSocketAddress successor;
 	private HashMap<Integer, InetSocketAddress> finger;
 
-    public static void main(String[] args) {
+
+    private void start() throws IOException {
+        /* The port on which the server should run */
+        int port = 9001;
+        server = ServerBuilder.forPort(port)
+            .addService(new DHTRpcImpl())
+            .build()
+            .start();
+        logger.info("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                DHTServer.this.stop();
+                System.err.println("*** server shut down");
+            }
+        });
+    }
+
+    private void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException{
         System.out.println("Server");
+        final DHTServer server = new DHTServer();
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+    static class DHTRpcImpl extends DHTRpcGrpc.DHTRpcImplBase {
+        @Override
+        public void findSuccessor(FindSuccessorRequest req, StreamObserver<FindSuccessorReply> responseObserver) {
+            int id = req.getId();
+            System.out.println("Received ID: " + id);
+            FindSuccessorReply reply = FindSuccessorReply.newBuilder().setAddress("HELLO").build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }   
     }
     
 }
