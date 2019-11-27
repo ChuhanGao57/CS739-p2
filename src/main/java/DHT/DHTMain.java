5f079de656e91644e00c4b3c3030532711eb0f46
ChuhanGao57 <gaochthu@gmail.com>
2019-11-27 10:36:17 -0600

    added test for query latency

--- src/main/java/DHT/DHTMain.java
+++ src/main/java/DHT/DHTMain.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.*;
 import java.util.*;
 
+
 public class DHTMain {
 
     private static Helper m_helper;
@@ -13,6 +14,23 @@ public class DHTMain {
         // long value = random.nextLong();
         //return lo + value % (hi - lo);
         return lo + (long) (Math.random() * (hi - lo));
+    }
+
+    public static String randomString() {
+  
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int) 
+              (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();
+     
+        return generatedString;
     }
     
     public static void testIAmPre()  throws IOException, InterruptedException {
@@ -67,45 +85,20 @@ public class DHTMain {
     public static void testQuery() throws UnknownHostException, IOException{
         m_helper = new Helper();
 
-        int numNode = 3;
-        int timeToSleep = 60 * 1000; // in ms
+        int numNode = 6;
+        int timeToSleep = 1 * 1000; // in ms
         List<InetSocketAddress> addrList = new ArrayList<>();
         List<DHTNode> nodeList = new ArrayList<>();
-        int startingPort = 8001;
-        for(int i = 0; i < numNode; i++) {
-            addrList.add(new InetSocketAddress("localhost", startingPort + i));
-            nodeList.add(new DHTNode(addrList.get(i)));
-        }
+        
+        
 
         
         try {
-            boolean successJoin = false;
-            for(int i = 0; i < numNode; i++) {
-                if(i == 0) {
-                    if(!nodeList.get(0).join(addrList.get(0))) {
-                        System.out.println("Cannot start ring");
-                        return;
-                    }
-                    
-                }
-                else {
-                    if(!nodeList.get(i).join(addrList.get(i-1))) {
-                        System.out.println("Node " + i + " cannot join ring");
-                        return;
-                    }
-                }
-
-                try {
-                    Thread.sleep(1000);
-                } catch(InterruptedException ex) {
-                    Thread.currentThread().interrupt();
-                }
+            
+            if(!buildRing(numNode, addrList, nodeList)) {
+                System.out.println("Build ring failed!");
+                return;
             }
-            System.out.println("Ring established");
-
-            nodeList.sort((DHTNode n1, DHTNode n2) -> Long.compare(n1.getId(), n2.getId()));
-            addrList.sort((InetSocketAddress a1, InetSocketAddress a2) -> Long.compare(Helper.hashSocketAddress(a1), Helper.hashSocketAddress(a2)));
-
             for(int i = 0; i < numNode; i++) {
                 if(!nodeList.get(i).getAddress().equals(addrList.get(i))) {
                     System.out.println("Address wrong: " + nodeList.get(i).getAddress() + " VS " + addrList.get(i));
@@ -133,7 +126,7 @@ public class DHTMain {
                         randomId = randomIdInRange(0, id2);
                     }
                     for(DHTNode node : nodeList) {
-                        if(!queryId(randomId, node.getAddress()).equals(node2.getAddress())) {
+                        if(queryId(randomId, node.getAddress()).getPort() != node2.getAddress().getPort()) {
                             errCnt += 1;
                             //System.out.println("Wrong query result");
                             //return;
@@ -159,10 +152,148 @@ public class DHTMain {
 
     }
 
+    public static void testQueryKeys() {
+        m_helper = new Helper();
+
+        int numNode = 10;
+        int numKey = 50;
+        int timeToSleep = 3 * 1000; // in ms
+        List<InetSocketAddress> addrList = new ArrayList<>();
+        List<DHTNode> nodeList = new ArrayList<>();
+        try {
+            if(!buildRing(numNode, addrList, nodeList)) {
+                System.out.println("Build ring failed!");
+                return;
+            }
+            // for(int i = 0; i < numNode; i++) {
+            //     if(!nodeList.get(i).getAddress().equals(addrList.get(i))) {
+            //         System.out.println("Address wrong: " + nodeList.get(i).getAddress() + " VS " + addrList.get(i));
+            //         return;
+            //     }
+            // }
+            long startTime = System.currentTimeMillis();
+            long lastTestTime = startTime;
+            // System.out.println("Sleeping " + timeToSleep/1000 + " sec before testing");
+            // System.out.println("Start testing");
+            while(true) {
+                lastTestTime = System.currentTimeMillis();
+                double accuracy = queryAccuracy(nodeList, numKey);
+                System.out.println("Time: " + (lastTestTime - startTime) / 1000 + "sec, Accuracy: " + accuracy + ", average query latency: " + (double)(System.currentTimeMillis() - lastTestTime) / numKey/numNode + "ms");
+                long currTime = System.currentTimeMillis();
+                if(currTime - startTime > 30 * 1000)
+                    break;
+                try {
+                    if(timeToSleep - (currTime - lastTestTime) > 0)
+                        Thread.sleep(timeToSleep - (currTime - lastTestTime));
+                } catch(InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+            
+            
+            
+            System.out.println("All tests completed");
+
+        } finally {
+            for(DHTNode node : nodeList) {
+                if(node != null)
+                    node.stopAllThreads();
+            }
+        }
+
+    }
+
+    private static double queryAccuracy(List<DHTNode> nodeList, int numKey) {
+        int numNode = nodeList.size();
+        TreeMap<Long, DHTNode> tree = buildTreeMap(nodeList);
+        int[] errCnt = new int[numNode];
+        int totalCnt = numKey * numNode;
+        for(int i = 0; i < numKey; i++) {
+            String randomKey = randomString();
+            InetSocketAddress nodeCorrect = correctQuery(randomKey, tree);
+            for(int j = 0; j < numNode; j++) {
+                DHTNode node = nodeList.get(j);
+                if(!queryId(Helper.hashString(randomKey), node.getAddress()).equals(nodeCorrect)) {
+                    errCnt[j] += 1;
+                    //System.out.println("Wrong query result");
+                    //return;
+                }
+            }
+        }
+        int errCntSum = 0;
+        for(int c : errCnt)
+            errCntSum += c;
+        //System.out.println(errCntSum + " out of " + totalCnt + " queries incorrect");
+        return 1 - (double) errCntSum / (double) totalCnt;
+    }
+
+    private static InetSocketAddress correctQuery(String key, TreeMap<Long, DHTNode> tree) {
+        // int numNode = tree.size();
+        long hash = Helper.hashString(key);
+        // long[] ids = new long[numNode];
+        // for(int i = 0; i < numNode; i++) {
+        //     ids[i] = nodeList.get(i).getId();
+        // }
+        // if(hash <= ids[0] || hash > ids[numNode - 1])
+        //     return nodeList.get(0).getAddress();
+        Long succId = tree.ceilingKey(hash);
+        if(succId == null) 
+            return tree.firstEntry().getValue().getAddress();
+        else
+            return tree.get(succId).getAddress();
+    }
+
+    private static boolean buildRing(int numNode, List<InetSocketAddress> addrList, List<DHTNode> nodeList) {
+        int startingPort = 8001;
+        for(int i = 0; i < numNode; i++) {
+            addrList.add(new InetSocketAddress("localhost", startingPort + i));
+            try {
+                nodeList.add(new DHTNode(addrList.get(i)));
+            } catch (IOException e) {
+				e.printStackTrace();
+			}
+        }
+        boolean successJoin = false;
+        for(int i = 0; i < numNode; i++) {
+            if(i == 0) {
+                if(!nodeList.get(0).join(addrList.get(0))) {
+                    System.out.println("Cannot start ring");
+                    return false;
+                }
+                
+            }
+            else {
+                if(!nodeList.get(i).join(addrList.get(i-1))) {
+                    System.out.println("Node " + i + " cannot join ring");
+                    return false;
+                }
+            }
+
+            try {
+                Thread.sleep(100);
+            } catch(InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        System.out.println("Ring established");
+
+        nodeList.sort((DHTNode n1, DHTNode n2) -> Long.compare(n1.getId(), n2.getId()));
+        addrList.sort((InetSocketAddress a1, InetSocketAddress a2) -> Long.compare(Helper.hashSocketAddress(a1), Helper.hashSocketAddress(a2)));
+        return true;
+    }
+
+    private static TreeMap<Long, DHTNode> buildTreeMap(List<DHTNode> nodeList) {
+        TreeMap<Long, DHTNode> tree = new TreeMap<>();
+        for(DHTNode node : nodeList) {
+            tree.put(node.getId(), node);
+        }
+        return tree;
+    }
+
 
     public static void main(String[] args) throws IOException, InterruptedException, UnknownHostException {
         
-        DHTMain.testQuery();
+        DHTMain.testQueryKeys();
 
     } 
 }
